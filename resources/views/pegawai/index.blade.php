d6c71b8769b27224041ea1f89b7e418f4150f55f
Aufa Billah <aufaroot18@gmail.com>
2019-03-06 17:52:12 +0700

    add column action: edit and delete

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -6,7 +6,15 @@
 	<link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
 </head>
 <body>
-	<div class="container mt-5">
+	<div class="container">
+		<h1 class="text-center mt-3">Data Pegawai</h1>
+		<a href="/pegawai/create" class="btn btn-primary mb-2">Tambah Data</a>
+		<form action="" class="form-inline float-right">
+			<div class="form-group mr-2">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+			<button type="submit" class="btn btn-primary">Search</button>
+		</form>
 		<table class="table table-bordered table-hover">
 			<thead class="thead-light">
 				<tr>
@@ -15,6 +23,7 @@
 					<th>Jabatan</th>
 					<th>Umur</th>
 					<th>Alamat</th>
+					<th>Opsi</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -25,6 +34,7 @@
 						<td>{{$p->jabatan}}</td>
 						<td>{{$p->umur}}</td>
 						<td>{{$p->alamat}}</td>
+						<td><a href="pegawai/edit/{{$p->id}}" class="btn btn-warning btn-sm">Edit</a> <a href="pegawai/hapus/{{$p->id}}" class="btn btn-danger btn-sm">Hapus</a></td>
 					</tr>
 				@endforeach
 			</tbody>
