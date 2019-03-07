64d95e37d1722d58b6df38f96dbf1cda4001751d
Aufa Billah <aufaroot18@gmail.com>
2019-03-08 05:08:45 +0700

    Edit View: Create Edit View

---
+++ resources/views/pegawai/edit.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>{{$judul}}</title>
+	<link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+</head>
+<body>
+	<div class="container">
+		<h1 class="text-center mt-5">Edit Data Pegawai</h1>
+		<form action="/pegawai/update" method="POST">
+			@csrf
+			<div class="form-group">
+				<label for="nama">Nama</label>
+				<input class="form-control" type="text" name="nama" id="nama" placeholder="Masukan Nama" value="{{$user->nama}}">
+				@error('nama')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
+			</div>
+			<div class="form-group">
+				<label for="jabatan">Jabatan</label>
+				<input class="form-control" type="text" name="jabatan" id="jabatan" placeholder="Masukan Jabatan" value="{{$user->jabatan}}">
+				@error('jabatan')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
+			</div>
+			<div class="form-group">
+				<label for="umur">Umur</label>
+				<input class="form-control" type="text" name="umur" id="umur" placeholder="Masukan Umur" value="{{$user->umur}}">
+				@error('umur')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
+			</div>
+			<div class="form-group">
+				<label for="alamat">Alamat</label>
+				<input class="form-control" type="text" name="alamat" id="alamat" placeholder="Masukan Alamat" value="{{$user->alamat}}">
+				@error('alamat')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
+			</div>
+			<button type="submit" class="btn btn-primary btn-block">Tambah Data</button>
+		</form>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+</body>
+</html>
