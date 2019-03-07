c7173ab3ad6d6f14f7a812202581fd869612e300
Aufa Billah <aufaroot18@gmail.com>
2019-03-07 18:16:28 +0700

    Display validation

--- resources/views/pegawai/create.blade.php
+++ resources/views/pegawai/create.blade.php
@@ -13,18 +13,30 @@
 			<div class="form-group">
 				<label for="nama">Nama</label>
 				<input class="form-control" type="text" name="nama" id="nama" placeholder="Masukan Nama">
+				@error('nama')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
 			<div class="form-group">
 				<label for="jabatan">Jabatan</label>
 				<input class="form-control" type="text" name="jabatan" id="jabatan" placeholder="Masukan Jabatan">
+				@error('jabatan')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
 			<div class="form-group">
 				<label for="umur">Umur</label>
 				<input class="form-control" type="text" name="umur" id="umur" placeholder="Masukan Umur">
+				@error('umur')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
 			<div class="form-group">
 				<label for="alamat">Alamat</label>
 				<input class="form-control" type="text" name="alamat" id="alamat" placeholder="Masukan Alamat">
+				@error('alamat')
+				    <div class="mt-3 alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
 			<button type="submit" class="btn btn-primary">Tambah Data</button>
 		</form>
